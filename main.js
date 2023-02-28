b3995135910ee4c8ead5920ca764bdb79668d771
jihoon.song <jihoon.song@medisolution.co.kr>
2023-02-28 18:01:49 +0900

    정적파일 서비스활용, 에러처리, 라우터 주소체계변경

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ app.get('*', function(request, response, next){
     });
 });
 
+
+
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // })
@@ -36,48 +38,18 @@ app.get('/', function (request, response) { //사실상 이 콜백 함수는 미
     var html = template.HTML(title, list,
         `<h2>${title}</h2>${description}
               <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px">`,
-        `<a href="/create">create</a>`
+        `<a href="/topic/create">create</a>`
     );
     // response.writeHead(200);
     // response.end(html);
     response.send(html);
 });
 
-//url path에 get으로 parameter 전달
-app.get('/page/:pageId', function (request, response, next) {
-    // console.log(request.list);  //['Express', 'HTML'] 배열
-
-    var filteredId = path.parse(request.params.pageId).base;
-
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-        if(err){
-            next(err);
-        }else{
-            var title = request.params.pageId;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {
-                allowedTags:['h1']
-            });
-            var list = template.list(request.list);
-            var html = template.HTML(sanitizedTitle, list,
-                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                ` <a href="/create">create</a>
-                         <a href="/update/${sanitizedTitle}">update</a>
-                         <form action="/delete_process" method="post">
-                            <input type="hidden" name="id" value="${sanitizedTitle}">
-                            <input type="submit" value="delete">
-                         </form>`
-            );
-            response.send(html);
-        }
-    });
-});
-
-app.get('/create', function (request, response){
+app.get('/topic/create', function (request, response){
     var title = 'WEB - create';
     var list = template.list(request.list);
     var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
+      <form action="/topic/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -90,7 +62,7 @@ app.get('/create', function (request, response){
     response.send(html);
 });
 
-app.post('/create_process', function(request, response){
+app.post('/topic/create_process', function(request, response){
     /*var body = '';
     request.on('data', function(data){
         body = body + data;
@@ -108,19 +80,18 @@ app.post('/create_process', function(request, response){
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-    })
-})
+        response.redirect(`/topic/${title}`);
+    });
+});
 
-app.get('/update/:pageId', function (request, response){
+app.get('/topic/update/:pageId', function (request, response){
     var filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
         var title = request.params.pageId;
         var list = template.list(request.list);
         var html = template.HTML(title, list,
             `
-        <form action="/update_process" method="post">
+        <form action="/topic/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
@@ -131,13 +102,14 @@ app.get('/update/:pageId', function (request, response){
           </p>
         </form>
         `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+            `<a href="/topic/create">create</a>
+                    <a href="/topic/update/${title}">update</a>`
         );
         response.send(html);
     });
 });
 
-app.post('/update_process', function(request, response){
+app.post('/topic/update_process', function(request, response){
     var post = request.body;
     var id = post.id;
     var title = post.title;
@@ -146,12 +118,12 @@ app.post('/update_process', function(request, response){
         fs.writeFile(`data/${title}`, description, 'utf8', function(err){
             // response.writeHead(302, {Location: `/?id=${title}`});
             // response.end();
-            response.redirect('/?id=${title}');
+            response.redirect(`/topic/${title}`);
         })
     });
 });
 
-app.post('/delete_process', function (request, response){
+app.post('/topic/delete_process', function (request, response){
     var post = request.body;
     var id = post.id;
     var filteredId = path.parse(id).base;
@@ -162,6 +134,37 @@ app.post('/delete_process', function (request, response){
         response.redirect('/');
     })
 });
+
+//url path에 get으로 parameter 전달
+app.get('/topic/:pageId', function (request, response, next) {
+    // console.log(request.list);  //['Express', 'HTML'] 배열
+
+    var filteredId = path.parse(request.params.pageId).base;
+
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+        if(err){
+            next(err);
+        }else{
+            var title = request.params.pageId;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+                allowedTags:['h1']
+            });
+            var list = template.list(request.list);
+            var html = template.HTML(sanitizedTitle, list,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+                ` <a href="/topic/create">create</a>
+                         <a href="/topic/update/${sanitizedTitle}">update</a>
+                         <form action="/topic/delete_process" method="post">
+                            <input type="hidden" name="id" value="${sanitizedTitle}">
+                            <input type="submit" value="delete">
+                         </form>`
+            );
+            response.send(html);
+        }
+    });
+});
+
 
 //미들웨어는 순차적으로 처리하기 때문에 에러처리는 마지막 쪽에 위치!!!
 app.use( (req, res, next) => {
