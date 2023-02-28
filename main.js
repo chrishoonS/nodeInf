3d2ac4cb9bdf07f40103dcff0a72ad8932fc1114
jihoon.song <jihoon.song@medisolution.co.kr>
2023-02-28 16:16:08 +0900

    body-parser, compression middleware 사용

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
 var sanitizeHtml = require('sanitize-html');
+var bodyParser = require('body-parser');  //body data를 parse 해주는 미들웨어
+var compression = require('compression'); //요청한 데이터를 압축하는 것이 네트워크 자원 절약해줌
+
+// parse application/x-www-form-urlencoded
+//사용자가 전달한 post data를 가져와 내부에서 분석해서 request 후 콜백 함수 호출
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression()); //웹브라우저가 compression을 통해 받은 압축된 데이터를 풀어서 사용
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
@@ -73,7 +80,7 @@ app.get('/create', function (request, response){
 });
 
 app.post('/create_process', function(request, response){
-    var body = '';
+    /*var body = '';
     request.on('data', function(data){
         body = body + data;
     });
@@ -85,7 +92,14 @@ app.post('/create_process', function(request, response){
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
         })
-    });
+    });*/
+    var post = request.body;
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end();
+    })
 })
 
 app.get('/update/:pageId', function (request, response){
@@ -116,82 +130,32 @@ app.get('/update/:pageId', function (request, response){
 });
 
 app.post('/update_process', function(request, response){
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-                // response.writeHead(302, {Location: `/?id=${title}`});
-                // response.end();
-                response.redirect('/?id=${title}');
-            })
-        });
+    var post = request.body;
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function(error){
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            // response.writeHead(302, {Location: `/?id=${title}`});
+            // response.end();
+            response.redirect('/?id=${title}');
+        })
     });
 });
 
 app.post('/delete_process', function (request, response){
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var filteredId = path.parse(id).base;
-        fs.unlink(`data/${filteredId}`, function(error){
-            //redirection 기능
-            // response.writeHead(302, {Location: `/`});
-            // response.end();
-            response.redirect('/');
-        })
-    });
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+        //redirection 기능
+        // response.writeHead(302, {Location: `/`});
+        // response.end();
+        response.redirect('/');
+    })
 });
 
 //listen이 실행되면서 web server를 인자로 넘겨준 port로 실행
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
-/*
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-
-    var body = '';
-
-    if(pathname === '/'){
-      if(queryData.id === undefined){
-      } else {
-      }
-    } else if(pathname === '/create'){
-
-    } else if(pathname === '/create_process'){
-
-
-    } else if(pathname === '/update'){
-    } else if(pathname === '/update_process'){
-
-    } else if(pathname === '/delete_process'){
-
-    } else {
-      response.writeHead(404);
-      response.end('Not found');
-    }
-});
-app.listen(3000);
-*/
